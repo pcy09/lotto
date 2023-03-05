6ac3a0ead4a389c37a96e17d94c50cf69662e64a
pcy09 <110226576+pcy09@users.noreply.github.com>
2023-03-05 12:39:59 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -18,13 +18,43 @@ const pickNum = () => {
 	bonus = suffled[suffled.length - 1];
 };
 
-let result_button = document.querySelector(".pick");
+let result_button = document.querySelector(".lottoPick");
 let numListGroup = document.querySelector(".lottoGroup");
 let bonusNum = document.querySelector(".bonusNum span");
 result_button.addEventListener("click", () => {
 	pickNum();
 	for (let i = 0; i < pickedNum.length; i++) {
-		numListGroup.children[i].textContent = pickedNum[i];
+		if (pickedNum[i] <= 10) {
+			numListGroup.children[i].style.backgroundColor = "#FBC400";
+			numListGroup.children[i].textContent = pickedNum[i];
+		} else if (pickedNum[i] <= 20) {
+			numListGroup.children[i].style.backgroundColor = "#69C8F2";
+			numListGroup.children[i].textContent = pickedNum[i];
+		} else if (pickedNum[i] <= 30) {
+			numListGroup.children[i].style.backgroundColor = "#FE7272";
+			numListGroup.children[i].textContent = pickedNum[i];
+		} else if (pickedNum[i] <= 40) {
+			numListGroup.children[i].style.backgroundColor = "#AAAAAA";
+			numListGroup.children[i].textContent = pickedNum[i];
+		} else {
+			numListGroup.children[i].style.backgroundColor = "#B0D840";
+			numListGroup.children[i].textContent = pickedNum[i];
+		}
 	}
-	bonusNum.textContent = bonus;
+	if (bonus <= 10) {
+		bonusNum.style.backgroundColor = "#FBC400";
+		bonusNum.textContent = bonus;
+	} else if (bonus <= 20) {
+		bonusNum.style.backgroundColor = "#69C8F2";
+		bonusNum.textContent = bonus;
+	} else if (bonus <= 30) {
+		bonusNum.style.backgroundColor = "#FE7272";
+		bonusNum.textContent = bonus;
+	} else if (bonus <= 40) {
+		bonusNum.style.backgroundColor = "#AAAAAA";
+		bonusNum.textContent = bonus;
+	} else if (bonus <= 45) {
+		bonusNum.style.backgroundColor = "#B0D840";
+		bonusNum.textContent = bonus;
+	}
 });
